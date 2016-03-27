c05a7f144b3ca41ba0a4a097d3d4d80746139ddb
Andrew Gaspar <andrew.gaspar@outlook.com>
2016-03-26 23:41:15 -0700

    Added an F# project to show that transducers are compatible with F#

---
+++ Samples/FSharp/Program.fs
@@ -0,0 +1,13 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+
+open TD;
+
+let parse = Standard.Parsing<uint64>()
+let inc = Core.Mapping(fun (x: uint64) -> x + 1UL)
+let even = Core.Filtering(fun x -> x % 2UL = 0UL)
+
+[<EntryPoint>]
+let main argv = 
+    System.Console.In.Transduce(System.Console.Out, parse.Compose(inc).Compose(even));
+    0 // return an integer exit code
